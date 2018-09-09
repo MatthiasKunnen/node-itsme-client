b99e1142f4cefbc34f961d8e9d7cf76d9bb56a5f
Matthias Kunnen <matthias.kunnen@gmail.com>
2018-09-09 22:34:45 +0200

    Structured exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
+// Identity provider
+export { IdentityProvider } from './identity-provider';
+
+// ItsmeClient
 export {
     ItsmeRdpConfiguration,
     ItsmeDiscoveryConfiguration,
 } from './interfaces/itsme-configuration.interface';
 export { ItsmeClient } from './itsme-client';
+
+// JWT
 export { JwkSet } from './interfaces/jwk-set.interface';
 export { Jwk } from './interfaces/jwk.interface';
-export { IdentityProvider } from './identity-provider';
+export { JwtPayload } from './interfaces/jwt.interface';
+
+// Util
 export {
     createKeyStore,
 } from './util';
+
+// Responses
 export {
     TokenResponse,
 } from './interfaces/token.interface';
-export { JwtPayload } from './interfaces/jwt.interface';
 export { UserInfoClaims } from './interfaces/claims.interface';
